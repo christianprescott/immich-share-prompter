ca41c95ef17a1bb0f47ebdd51c785f0364674e9a
Christian Prescott <me@christianprescott.com>
2026-09-15 11:43:18 -0600

    Add @immich/ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
+	import '../app.css';
+
 	import favicon from '$lib/assets/favicon.svg';
+	import {
+		AppShell,
+		AppShellHeader,
+		Button,
+		Constants,
+		Logo,
+		ThemeSwitcher,
+		TooltipProvider
+	} from '@immich/ui';
+	import { mdiOpenInNew } from '@mdi/js';
+	import { siGithub } from 'simple-icons';
 
 	let { children } = $props();
 </script>
@@ -8,4 +21,31 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<TooltipProvider>
+	<AppShell>
+		<AppShellHeader>
+			<div class="w-full">
+				<nav class="flex justify-between p-2 lg:gap-2">
+					<div class="flex place-items-center gap-2">
+						<a href="/" class="flex gap-2 text-4xl">
+							<Logo variant="inline" />
+						</a>
+					</div>
+					<div class="flex place-items-center justify-end gap-2">
+						<Button
+							leadingIcon={siGithub.path}
+							trailingIcon={mdiOpenInNew}
+							color="secondary"
+							variant="ghost"
+						>
+							GitHub
+						</Button>
+						<ThemeSwitcher />
+					</div>
+				</nav>
+			</div>
+		</AppShellHeader>
+
+		{@render children()}
+	</AppShell>
+</TooltipProvider>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,18 @@
-<script>
+<script lang="ts">
+	import { Container, Heading, Link, Stack, Text } from '@immich/ui';
+
 	const { data } = $props();
 </script>
 
-<p>
-	Visit <a href={data.immichHost}>your Immich instance</a>
-</p>
-<p>
-	Maybe you want to share a photo from {data.cities.join(', ')}
-</p>
+<Container size="medium" center class="mt-8 mb-24 p-4 lg:p-8">
+	<Stack gap={4}>
+		<Heading size="giant" tag="h1">Immich Share Prompter</Heading>
+
+		<Text>
+			Visit <Link href={data.immichHost}>your Immich instance</Link>
+		</Text>
+		<Text>
+			Maybe you want to share a photo from {data.cities.join(', ')}
+		</Text>
+	</Stack>
+</Container>
