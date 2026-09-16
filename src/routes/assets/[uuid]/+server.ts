a047fbb39cb8bc3779125a023b0f206987bd5d6f
Christian Prescott <me@christianprescott.com>
2026-09-15 23:17:37 -0600

    Add immich asset proxy

---
+++ src/routes/assets/[uuid]/+server.ts
@@ -0,0 +1,13 @@
+import { defaults } from '$lib/server/immich';
+import type { RequestHandler } from '@sveltejs/kit';
+
+// Requests for Immich assets must be authenticated. Proxy requests so <img>
+// elements can use a regular GET request for their src
+export const GET: RequestHandler = async ({ params, fetch }) => {
+const asset = await fetch(`${defaults.baseUrl}/assets/${params.uuid}/thumbnail?size=preview`, {
+		headers: defaults.headers
+	});
+	return new Response(asset.body, {
+		headers: { 'Content-Type': asset.headers.get('Content-Type') }
+	});
+};
--- src/routes/share/+page.svelte
+++ src/routes/share/+page.svelte
@@ -1,9 +1,12 @@
 <script>
 	import { Button, Container, Field, Input, Stack } from '@immich/ui';
+	const { data } = $props();
 </script>
 
 <Container size="medium" center class="mt-8 mb-24 p-4 lg:p-8">
 	<Stack gap={4}>
+		{data.asset.id}
+		<img src="/assets/{data.asset.id}" />
 		<form method="POST" action="/share">
 			<Field label="name">
 				<Input name="name" value="Tom" />
