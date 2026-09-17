f77752e5c72b9982a17cf8856bdda9b416c6027d
Christian Prescott <me@christianprescott.com>
2026-09-17 10:42:23 -0600

    Fetch suggested assets

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,8 +2,32 @@ import ImmichClient from '$lib/server/immich';
 
 export async function load() {
 	const client = new ImmichClient();
+	const assetsFromPlace = client
+		.searchRandom({
+			randomSearchDto: {
+				size: 3,
+				withExif: true,
+				filter: {
+					type: { eq: AssetTypeEnum.Image },
+					trashedAt: { eq: null },
+					city: { ne: null }
+				}
+			}
+		})
+		.then((assets) =>
+			assets.map(
+				(a) =>
+					({
+						title: a.exifInfo?.city,
+						href: `/share/${a.id}`,
+						src: `/assets/${a.id}`,
+						rightIcons: [mdiMap]
+					}) as CarouselImageItem
+			)
+		);
 	const markers = await client.getMapMarkers({});
 	return {
+		assets: assetsFromPlace,
 		cities: [...new Set(markers.map((m) => m.city).filter((c) => !!c))]
 	};
 }
