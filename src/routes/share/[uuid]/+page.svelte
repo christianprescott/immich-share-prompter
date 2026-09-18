a51bd83ca5d2d574452b3463f811b0f94fc87c00
Christian Prescott <me@christianprescott.com>
2026-09-17 19:06:37 -0600

    Add view in timeline button

--- src/routes/share/[uuid]/+page.svelte
+++ src/routes/share/[uuid]/+page.svelte
@@ -1,12 +1,22 @@
 <script>
-	import { Button, Container, Field, Input, Stack } from '@immich/ui';
+	import { IMMICH_HOST } from '$app/env/public';
+	import { Button, Container, Field, Input, Stack, Logo } from '@immich/ui';
+	import { mdiOpenInNew } from '@mdi/js';
 	const { params } = $props();
 </script>
 
 <Container size="medium" center class="mt-8 mb-24 p-4 lg:p-8">
 	<Stack gap={4}>
-		{params.uuid}
-		<img src="/assets/{params.uuid}" />
+		<div class="relative rounded-xl overflow-hidden">
+			<img src="/assets/{params.uuid}" />
+			<Button
+				href={`${IMMICH_HOST}/photos?at=${params.uuid}`}
+				trailingIcon={mdiOpenInNew}
+				color="secondary"
+				variant="outline"
+				class="absolute top-2 inset-e-2"><Logo variant="icon" size="tiny" />View in timeline</Button
+			>
+		</div>
 		<form method="POST" action="/share/{params.uuid}">
 			<Field label="Your name">
 				<Input name="name" value="Tom" />
