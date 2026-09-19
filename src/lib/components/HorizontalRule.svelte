70beee64c90929fd8fcb32418f3df19a690148f3
Christian Prescott <me@christianprescott.com>
2026-09-19 00:44:11 -0600

    Add share button

---
+++ src/lib/components/HorizontalRule.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	const { children } = $props();
+</script>
+
+<div class="separator">{@render children()}</div>
+
+<style>
+	.separator {
+		display: flex;
+		align-items: center;
+		text-align: center;
+	}
+	.separator::before,
+	.separator::after {
+		content: '';
+		flex: 1;
+		border-bottom: 1px solid currentColor;
+		margin: auto;
+	}
+	.separator:not(:empty)::before {
+		margin-right: 0.5em;
+	}
+	.separator:not(:empty)::after {
+		margin-left: 0.5em;
+	}
+</style>
--- src/routes/share/[uuid]/+page.svelte
+++ src/routes/share/[uuid]/+page.svelte
@@ -12,12 +12,47 @@
 		IconButton,
 		HelperText,
 		Card,
-		CardBody
+		CardBody,
+		Text
 	} from '@immich/ui';
-	import { mdiOpenInNew, mdiSend } from '@mdi/js';
+	import { mdiOpenInNew, mdiSend, mdiShareVariant } from '@mdi/js';
+	import HorizontalRule from '$lib/components/HorizontalRule.svelte';
 	const { params, form } = $props();
 
 	let submitting = $state(false);
+
+	// Call the function lazily and at most once, reusing the promise result to
+	// avoid extra image fetch.
+	let fetchAssetFilePromise;
+	function fetchAssetFile() {
+		fetchAssetFilePromise ??= (async () => {
+			const response = await fetch(`/assets/${params.uuid}`);
+			const blob = await response.blob();
+			const extension = blob.type.split('/')[1] ?? 'jpg';
+			return new File([blob], `${params.uuid}.${extension}`, { type: blob.type });
+		})();
+		return fetchAssetFilePromise;
+	}
+
+	// `share({ files })` Works in mobile Chrome, but not Firefox nor desktop.
+	// Still worth doing but most browsers must right-click-on-image for now.
+	async function canShareFiles() {
+		if (!navigator.canShare) return false;
+		const file = await fetchAssetFile();
+		return navigator.canShare({ files: [file] });
+	}
+
+	let sharing = $state(false);
+	async function shareImage() {
+		sharing = true;
+		try {
+			const file = await fetchAssetFile();
+			await navigator.share({ files: [file] });
+		} catch {
+		} finally {
+			sharing = false;
+		}
+	}
 </script>
 
 <Container size="medium" center class="mb-24 p-4 lg:p-8">
@@ -82,6 +117,23 @@
 						</Button>
 					</HStack>
 				</form>
+				{#await canShareFiles() then canShare}
+					{#if canShare}
+						<Text color="muted" fontWeight="light" size="tiny">
+							<HorizontalRule>OR</HorizontalRule>
+						</Text>
+
+						<Button
+							size="large"
+							shape="round"
+							variant="outline"
+							fullWidth
+							leadingIcon={mdiShareVariant}
+							onclick={shareImage}
+							disabled={sharing}>Share</Button
+						>
+					{/if}
+				{/await}
 			</Stack>
 		</CardBody>
 	</Card>
