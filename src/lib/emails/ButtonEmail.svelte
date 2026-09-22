e13b9ef5e905506ca050fd391f78d7046f243b48
Christian Prescott <me@christianprescott.com>
2026-09-22 10:23:33 -0600

    Prompt email style and layout

--- src/lib/emails/ButtonEmail.svelte
+++ src/lib/emails/ButtonEmail.svelte
@@ -1,9 +1,45 @@
 <script lang="ts">
 	import { IMMICH_HOST } from '$app/env/public';
 	import { Button, ImageCard } from '@immich/ui';
+
+	const assetId = 'dac8f9ca-750a-4b2f-b4d5-0489ddbab355';
 </script>
 
-<ImageCard item={{title: '', src: "/assets/dac8f9ca-750a-4b2f-b4d5-0489ddbab355", href: "/share/dac8f9ca-750a-4b2f-b4d5-0489ddbab355"}} />
-<Button color="primary" shape="round" href={IMMICH_HOST}>View in timeline</Button>
-<Button color="secondary" shape="semi-round" href={IMMICH_HOST}>More suggestions</Button>
-<Button color="secondary" variant="ghost" href={IMMICH_HOST}>More suggestions</Button>
+<table role="presentation" width="100%">
+	<tbody>
+		<tr>
+			<td align="center">
+				<table role="presentation" align="center">
+					<tbody>
+						<tr>
+							<td style="padding-bottom: 8px;">
+								<ImageCard
+									item={{
+										href: `/share/${assetId}`,
+										src: `/assets/${assetId}`,
+										title: '',
+										alt: ''
+									}}
+								/>
+							</td>
+						</tr>
+						<tr>
+							<td style="padding-bottom: 8px; text-align: center;">
+								<Button color="primary" shape="semi-round" href={`/share/${assetId}`} fullWidth
+									>Share</Button
+								>
+							</td>
+						</tr>
+						<tr>
+							<td style="padding-bottom: 8px; text-align: center;">
+								<Button color="secondary" variant="ghost" shape="semi-round" href={'/'} fullWidth
+									>More suggestions</Button
+								>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</td>
+		</tr>
+	</tbody>
+</table>
